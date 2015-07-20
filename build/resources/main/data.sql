a688b3020086be25090714bccb3ce2fb0ca74395
Brandon Carter <bcarter1@gmail.com>
2015-07-19 23:55:46 -0400

    remove folders

--- build/resources/main/data.sql
+++
@@ -1,2 +0,0 @@
-insert into Parent (id, email, firstName, lastName, password, username)  values (1, 'test@test.com', 'Test', 'Parent', 'password', 'test1234');
-insert into Child (id, balance, firstName, lastName, institution, state, studentId, parentId)  values (1, 10.25, 'Test', 'Child1', 'White Oak Elementary', 'GA', 'GA-12345', 1);
