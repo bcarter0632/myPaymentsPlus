f21423bbe3a3c46964a85be408d4d2b52a77c063
Brandon Carter <bcarter1@gmail.com>
2015-07-20 00:24:26 -0400

    Removed folder from repository

--- bin/data.sql
+++
@@ -1,3 +0,0 @@
-insert into Parent (id, email, firstName, lastName, password, username)  values (1, 'test@test.com', 'Test', 'Parent', 'password', 'test1234');
-insert into Child (id, balance, firstName, lastName, institution, state, studentId, parentId)  values (1, 10.25, 'Test', 'Child1', 'White Oak Elementary', 'GA', 'GA-12345', 1);
-insert into CreditCard (id, name, cardNumber, ccv, expiration, parentId)  values (1, 'First Payment Profile', '1112333344445555', '012', '09/2017', 1);
